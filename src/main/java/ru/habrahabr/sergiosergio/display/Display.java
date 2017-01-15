ae133c5eb58daa57607175b9928c526ff8b3e64b
Sergei Grimanov <tosergi@gmail.com>
2017-01-16 00:05:51 +0300

    16.01.17

--- src/main/java/ru/habrahabr/sergiosergio/display/Display.java
+++ src/main/java/ru/habrahabr/sergiosergio/display/Display.java
@@ -1,8 +1,14 @@
 package ru.habrahabr.sergiosergio.display;
 
-import javax.swing.*;
-import java.awt.Canvas;
+
+import javax.swing.JFrame;
 import java.awt.Dimension;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.awt.image.DataBuffer;
+import java.util.Arrays;
 
 /**
  * Created by sg on 14.01.2017.
@@ -13,7 +19,12 @@ public abstract class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create (int width, int height, String title){
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    public static void create (int width, int height, String title, int _clearColor){
 
         if(created){
 
@@ -21,7 +32,7 @@ public abstract class Display {
         }
 
         window = new JFrame(title);
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         content = new Canvas();
 
         Dimension size = new Dimension(width, height);
@@ -31,5 +42,25 @@ public abstract class Display {
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
+
+        created = true;
+    }
+
+    public static void clear(){
+        Arrays.fill(bufferData, clearColor);
+    }
+
+    public static void render(){
+
+    }
+
+    public static void swapBuffers(){
+        Graphics g = content.getGraphics();
+        g.drawImage(buffer, 0, 0, null);
     }
 }
--- src/main/java/ru/habrahabr/sergiosergio/main/Main.java
+++ src/main/java/ru/habrahabr/sergiosergio/main/Main.java
@@ -2,6 +2,10 @@ package ru.habrahabr.sergiosergio.main;
 
 import ru.habrahabr.sergiosergio.display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.Timer;
+
 /**
  * Created by sg on 14.01.2017.
  */
@@ -9,6 +13,15 @@ public class Main {
 
     public static void main(String[] args){
 
-        Display.create(800, 600, "Tanks!");
+        Display.create(800, 600, "Tanks!", 0xff00ff00);
+
+        Timer t = new Timer(1000 / 60, new AbstractAction(){
+
+            public void actionPerformed(ActionEvent e){
+                Display.clear();
+                Display.render();
+                Display.swapBuffers();
+            }
+        }
     }
 }
